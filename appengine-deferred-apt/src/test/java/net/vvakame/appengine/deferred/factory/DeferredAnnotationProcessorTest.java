745346e03110cbbc54afed266f92382bd0a4e6f9
vvakame <vvakame+dev@gmail.com>
2011-04-18 14:19:27 +0900

    artifactIdの変更

---
+++ appengine-deferred-apt/src/test/java/net/vvakame/appengine/deferred/factory/DeferredAnnotationProcessorTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2011 vvakame <vvakame@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.vvakame.appengine.deferred.factory;
+
+import net.vvakame.appengine.deferred.sample.SampleService;
+
+import org.junit.Test;
+import org.seasar.aptina.unit.AptinaTestCase;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * {@link DeferredAnnotationProcessor} のテスト.
+ * 
+ * @author vvakame
+ */
+public class DeferredAnnotationProcessorTest extends AptinaTestCase {
+
+	/**
+	 * 動作確認.
+	 * 
+	 * @throws Exception
+	 * @author vvakame
+	 */
+	@Test
+	public void test() throws Exception {
+		DeferredAnnotationProcessor processor = new DeferredAnnotationProcessor();
+		addProcessor(processor);
+
+		addCompilationUnit(SampleService.class);
+
+		compile();
+
+		@SuppressWarnings("unused")
+		String source = getGeneratedSource(SampleService.class.getName()
+				+ "Deferred");
+
+		assertThat(getCompiledResult(), is(true));
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		// ソースパスを追加
+		addSourcePath("src/test/java");
+		setCharset("utf-8");
+	}
+}
---
+++ appengine-deferred-apt/src/test/java/net/vvakame/appengine/deferred/sample/SampleService.java
@@ -0,0 +1,180 @@
+package net.vvakame.appengine.deferred.sample;
+
+import java.util.ConcurrentModificationException;
+import java.util.List;
+import java.util.Map;
+
+import net.vvakame.appengine.deferred.annotation.Deferred;
+import net.vvakame.appengine.deferred.util.DeferredUtil;
+
+import com.google.appengine.api.datastore.Key;
+
+/**
+ * なんか適当な処理.
+ * 
+ * @author vvakame
+ */
+public class SampleService {
+
+	static int count = 0;
+
+	/**
+	 * テスト用メソッド
+	 * 
+	 * @param sample
+	 * @return 適当
+	 * @author vvakame
+	 */
+	@Deferred
+	public static int hoge(long sample) {
+		try {
+			if ((count % 4) == 0) {
+				count += sample;
+				throw new IllegalStateException();
+			} else {
+				count += sample;
+			}
+		} catch (IllegalStateException e) {
+			DeferredUtil.throwWithValue(count, "error!", e);
+		}
+		return count;
+	}
+
+	/**
+	 * テスト用メソッド
+	 * 
+	 * @param keyBy
+	 * @return 適当
+	 * @author vvakame
+	 */
+	@Deferred
+	public static List<Key> getKeys(Key keyBy) {
+		return null;
+	}
+
+	/**
+	 * テスト用メソッド
+	 * 
+	 * @param key
+	 * @param keys
+	 * @return 適当
+	 * @author vvakame
+	 */
+	@Deferred
+	public static Map<String, List<? extends Number>> generics(Key key,
+			List<Key> keys) {
+
+		return null;
+	}
+
+	/**
+	 * テスト用メソッド
+	 * 
+	 * @author vvakame
+	 * @throws ConcurrentModificationException
+	 */
+	@Deferred
+	public static void throwRuntimeException()
+			throws ConcurrentModificationException {
+
+	}
+
+	/**
+	 * テスト用メソッド
+	 * 
+	 * @throws Exception
+	 * @author vvakame
+	 */
+	@Deferred
+	public static void throwException() throws Exception {
+
+	}
+
+	/**
+	 * テスト用メソッド
+	 * 
+	 * @return 適当
+	 * @author vvakame
+	 */
+	@Deferred
+	public static boolean returnBoolean() {
+		return false;
+	}
+
+	/**
+	 * テスト用メソッド
+	 * 
+	 * @return 適当
+	 * @author vvakame
+	 */
+	@Deferred
+	public static char returnChar() {
+		return 'a';
+	}
+
+	/**
+	 * テスト用メソッド
+	 * 
+	 * @return 適当
+	 * @author vvakame
+	 */
+	@Deferred
+	public static byte returnByte() {
+		return 0;
+	}
+
+	/**
+	 * テスト用メソッド
+	 * 
+	 * @return 適当
+	 * @author vvakame
+	 */
+	@Deferred
+	public static short returnShort() {
+		return 0;
+	}
+
+	/**
+	 * テスト用メソッド
+	 * 
+	 * @return 適当
+	 * @author vvakame
+	 */
+	@Deferred
+	public static int returnInteger() {
+		return 0;
+	}
+
+	/**
+	 * テスト用メソッド
+	 * 
+	 * @return 適当
+	 * @author vvakame
+	 */
+	@Deferred
+	public static long returnLong() {
+		return 0;
+	}
+
+	/**
+	 * テスト用メソッド
+	 * 
+	 * @return 適当
+	 * @author vvakame
+	 */
+	@Deferred
+	public static Float returnFloat() {
+		return 0.0f;
+	}
+
+	/**
+	 * テスト用メソッド
+	 * 
+	 * @return 適当
+	 * @author vvakame
+	 */
+	@Deferred
+	public static Double returnDouble() {
+		return 0.0;
+	}
+}
